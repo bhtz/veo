80716ef192bc022a70a59c8fb04913dcb9b76afe
bhtz <heintz.benjamin@gmail.com>
2016-03-08 21:59:39 +0100

    preparing fb auth

--- server/middlewares/authentication.js
+++ server/middlewares/authentication.js
@@ -6,47 +6,59 @@ import User from '../models/User';
 
 // Use local strategy
 passport.use(new Strategy(
-	function (username, password, done) {
-		User.findOne({ username: username }, function (err, user) {
-			if (err) { return done(err); }
+    function(username, password, done) {
+        User.findOne({ username: username }, function(err, user) {
+            if (err) { return done(err); }
 
-			if (!user) {
-				return done(null, false, { message: 'Incorrect username.' });
-			}
+            if (!user) {
+                return done(null, false, { message: 'Incorrect username.' });
+            }
 
-			return user.comparePassword(password, (err, isMatch) => {
-				if (err) throw err;
-				if (isMatch) {
-					return done(null, user);
-				} else {
-					return done(null, false, { message: 'Incorrect password.' });
-				}
-			});
-		});
-	}
+            return user.comparePassword(password, (err, isMatch) => {
+                if (err) throw err;
+                if (isMatch) {
+                    return done(null, user);
+                } else {
+                    return done(null, false, { message: 'Incorrect password.' });
+                }
+            });
+        });
+    }
 ));
 
+// passport.use(new FacebookStrategy({
+//     clientID: FACEBOOK_APP_ID,
+//     clientSecret: FACEBOOK_APP_SECRET,
+//     callbackURL: "http://localhost:3000/auth/facebook/callback"
+// },
+//     function(accessToken, refreshToken, profile, cb) {
+//         User.findOrCreate({ facebookId: profile.id }, function(err, user) {
+//             return cb(err, user);
+//         });
+//     }
+// ));
+
 // serialize user
-passport.serializeUser(function (user, done) {
-	done(null, user.id);
+passport.serializeUser(function(user, done) {
+    done(null, user.id);
 });
 
 // deserialize user
-passport.deserializeUser(function (id, done) {
-	User.findById(id, function (err, user) {
-		done(err, user);
-	});
+passport.deserializeUser(function(id, done) {
+    User.findById(id, function(err, user) {
+        done(err, user);
+    });
 });
 
 // register middlewares 
 export function authentication(app) {
-	app.use(session({ secret: 'microscopejs', resave: false, saveUninitialized: false }));
-	app.use(flash());
-	app.use(passport.initialize());
-	app.use(passport.session());
-	app.use(function (req, res, next) {
-		res.locals.user = req.user;
-		res.locals.flash = req.flash('info');
-		next();
-	});
+    app.use(session({ secret: 'microscopejs', resave: false, saveUninitialized: false }));
+    app.use(flash());
+    app.use(passport.initialize());
+    app.use(passport.session());
+    app.use(function(req, res, next) {
+        res.locals.user = req.user;
+        res.locals.flash = req.flash('info');
+        next();
+    });
 }
